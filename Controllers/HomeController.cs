92f4ecd56a45ff3f1c27415e62b3e1f3536b42dd
Михаил Овчинников <swedisch97@gmail.com>
2019-12-07 18:46:30 +0300

    add Views and change controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkingWithVisualStudio.Models;
 
 namespace WorkingWithVisualStudio.Controllers
 {
-    public class HomeController
+    public class HomeController:Controller
     {
+        public IActionResult Index() => View(SimpleRepository.SharedRepository.Products);
     }
 }
